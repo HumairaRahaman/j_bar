41887c8ff141bcad1060ce0405792ef50622e1d8
HumairaRahaman <humairamitul@gmail.com>
2023-11-07 15:45:33 +0600

    jbar style,script,jquery loading

--- src/Extension/Jbar.php
+++ src/Extension/Jbar.php
@@ -10,11 +10,14 @@
 
 namespace Joomla\Plugin\System\Jbar\Extension;
 
+use JHtml;
+use Joomla\CMS\Event\Application\AfterInitialiseDocumentEvent;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\Event\DispatcherInterface;
+use Joomla\Event\Priority;
 use Joomla\Event\SubscriberInterface;
 use Joomla\CMS\Version;
 use Joomla\CMS\Uri\Uri;
+
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -24,64 +27,65 @@ use Joomla\CMS\Uri\Uri;
  *
  * @since  1.5
  */
-
-
 final class Jbar extends CMSPlugin implements SubscriberInterface
 {
-   
+
 
     public static function getSubscribedEvents(): array
     {
-     
+
         return [
-            'onAfterRender'  => ['onAfterRender'],
-            'onAfterInitialise' => ['onAfterInitialise'],
+            'onAfterInitialiseDocument' => ['onAfterInitialiseDocument', Priority::HIGH],
+            'onAfterRender' => ['onAfterRender'],
         ];
     }
 
-    function onAfterInitialise()
+    public function onAfterInitialiseDocument(AfterInitialiseDocumentEvent $event)
     {
         $app = $this->getApplication();
-        if( ! $app->isClient('site') ) return;
+        if (!$app->isClient('site')) return;
         $page = $this->params->get('load_page', 'all');
 
-        if( $page == 'homepage' )
-        {
+        $wa = $event->getDocument()
+            ->getWebAssetManager();
+        $style = $this->loadStyle();
+
+
+        if ($page == 'homepage') {
             $current_menu_id = $this->app->input->get('Itemid');
-            if( $current_menu_id == NULL )
-            {
+            if ($current_menu_id == NULL) {
                 // Load jQuery
                 $this->loadjQuery();
 
                 // Load style
-                $this->loadStyle();
+                $wa->addInlineStyle($style);
 
                 // Load Script
                 $this->loadScript();
             }
-            return ;
+            return;
         }
 
-        if( !$this->isLoadable() ) return;
+        if (!$this->isLoadable()) return;
 
         // Load jQuery
         $this->loadjQuery();
 
         // Load style
-        $this->loadStyle();
+        $wa->addInlineStyle($style);
 
         // Load Script
         $this->loadScript();
-    }
 
+    }
+    
     public function onAfterRender()
     {
         $app = $this->getApplication();
-
         if (!$app->isClient('site')) {
             return;
         }
-        if( !$this->isLoadable() ) return;
+        if (!$this->isLoadable()) return;
 
 
         $doc = $this->getApplication()->getDocument();
@@ -91,6 +95,9 @@ final class Jbar extends CMSPlugin implements SubscriberInterface
         if ($docType !== 'html') {
             return;
         }
+        $this->loadStyle();
+        $this->loadjQuery();
+        $this->loadScript();
 
         $body = $this->getApplication()->getBody();
 
@@ -99,41 +106,37 @@ final class Jbar extends CMSPlugin implements SubscriberInterface
         // Set some NULL variable
         $link = $target = $closable = $btn = '';
         // Check if the link is set to visible or not
-        if( $this->params->get('link_type', 1) )
-        {
+        if ($this->params->get('link_type', 1)) {
             $link_text = $this->params->get('link_text');
             $link_url = $this->params->get('link_url');
 
-            if( (int) $this->params->get('target') == 2)
-            {
+            if ((int)$this->params->get('target') == 2) {
                 $target = 'target="_blank"';
             }
 
-            if( (int) $this->params->get('link_type') == 2)
-            {
+            if ((int)$this->params->get('link_type') == 2) {
                 $btn = 'class="jBtn"';
             }
 
-            $link = ' <a href="'. $link_url .'" '. $target . $btn .'>'. $link_text .'</a>';
+            $link = ' <a href="' . $link_url . '" ' . $target . $btn . '>' . $link_text . '</a>';
         }
 
-        if( ! (int) $this->params->get('closable'))
-        {
+        if (!(int)$this->params->get('closable')) {
             $closable = ' jHide';
         }
 
-       $position = ' j' . $this->params->get('tab_position','right');
+        $position = ' j' . $this->params->get('tab_position', 'right');
 
         $jbar = '
         <!-- jBar by ThemeXpert.com 1.5 -->
         <div id="jBar">
             <div class="jBar">
                 <span class="helloinner">
-                    <p class="text">'. $message . $link .'</p>
-                    <span class="jTrigger downarrow arrow'. $closable . $position .'">Arrow up</span>
+                    <p class="text">' . $message . $link . '</p>
+                    <span class="jTrigger downarrow arrow' . $closable . $position . '">Arrow up</span>
                 </span>
             </div>
-            <span class="jRibbon jTrigger arrow'. $closable . $position .'">Arrow down</span>
+            <span class="jRibbon jTrigger arrow' . $closable . $position . '">Arrow down</span>
         </div>
         <!-- jBar by ThemeXpert.com 1.5 -->
     ';
@@ -145,14 +148,14 @@ final class Jbar extends CMSPlugin implements SubscriberInterface
         $body = str_replace($match[0], $match[0] . $jbar, $body);
 
         $this->getApplication()->setBody($body);
+
     }
 
     function isLoadable()
     {
         $page = $this->params->get('load_page', 'all');
 
-        switch( $page )
-        {
+        switch ($page) {
             case 'all':
             case 'default':
                 return TRUE;
@@ -162,10 +165,9 @@ final class Jbar extends CMSPlugin implements SubscriberInterface
                 $menu = $this->app->getMenu();
 
                 // Though initial method dosend found the active menu we have to compare with menu id
-                if ($menu->getActive() == $menu->getDefault())
-                {
+                if ($menu->getActive() == $menu->getDefault()) {
                     return TRUE;
-                }else{
+                } else {
                     return FALSE;
                 }
 
@@ -173,19 +175,17 @@ final class Jbar extends CMSPlugin implements SubscriberInterface
 
                 $ids = $this->params->get('load_page_id');
 
-                if(!empty($ids))
-                {
-                    $menu_ids = explode(',',$ids);
+                if (!empty($ids)) {
+                    $menu_ids = explode(',', $ids);
                     $current_menu_id = $this->app->input->get('Itemid');
-                    if(in_array($current_menu_id, $menu_ids))
-                    {
+                    if (in_array($current_menu_id, $menu_ids)) {
                         return TRUE;
 
-                    }else{
+                    } else {
                         return FALSE;
                     }
 
-                }else{
+                } else {
                     return FALSE;
                 }
 
@@ -195,13 +195,12 @@ final class Jbar extends CMSPlugin implements SubscriberInterface
     function loadStyle()
     {
         $doc = $this->getApplication()->getDocument();
-
         //Generate css and add to head
-        $bar_color      = '#' . $this->params->get('bar_color');
-        $text_color     = '#' . $this->params->get('text_color');
-        $link_color     = '#' . $this->params->get('link_color');
-        $border_color   = '#' . $this->params->get('border_color');
-        $font   = $this->params->get('font');
+        $bar_color = '#' . $this->params->get('bar_color');
+        $text_color = '#' . $this->params->get('text_color');
+        $link_color = '#' . $this->params->get('link_color');
+        $border_color = '#' . $this->params->get('border_color');
+        $font = $this->params->get('font');
 
         $image_path = Uri::root(true) . '/plugins/system/jbar/assets/';
         $arrow_down = $image_path . 'arrow-down.png';
@@ -209,13 +208,12 @@ final class Jbar extends CMSPlugin implements SubscriberInterface
 
         $position = 'relative';
         $jribbon_position = '';
-        if( $this->params->get('sticky', 0) )
-        {
+        if ($this->params->get('sticky', 0)) {
             $position = 'fixed';
             $jribbon_position = 'position: fixed';
         }
 
-        $css  = "#jBar .jBar {background: $bar_color; padding: 5px; width:100%; position:$position; z-index: 99999; }";
+        $css = "#jBar .jBar {background: $bar_color; padding: 5px; width:100%; position:$position; z-index: 99999; }";
         $css .= "#jBar .jBar p{color: $text_color; padding: 7px 0; text-align: center; margin: 0; font-family: $font}";
         $css .= "#jBar .jBar a{color: $link_color ;}";
 
@@ -231,82 +229,73 @@ final class Jbar extends CMSPlugin implements SubscriberInterface
 
         $css .= "#jBar .jHide{ display: none !important; }";
 
-        if( $this->params->get('border') )
-        {
+        if ($this->params->get('border')) {
             $css .= "#jBar .jBar{border-bottom: 3px solid $border_color ;}";
         }
 
-        if ($this->params->get('mobile_disabled', 1) )
-        {
+        if ($this->params->get('mobile_disabled', 1)) {
             $css .= "@media (max-width: 480px) {#jBar {display:none;} }";
         }
 
-        $doc->addStyleDeclaration($css);
+        return ($css);
     }
 
     function loadScript()
     {
-        $doc =  $this->getApplication()->getDocument();
+        $doc = $this->getApplication()->getDocument();
         $show_wait = $this->params->get('show_wait');
         $hide_after = $this->params->get('hide_after');
         $js_in = '';
 
-        if ( $show_wait != -1){
+        if ($show_wait != -1) {
             $js_in .= "jQuery('.jRibbon').delay(500).fadeIn(400).slideUp(600).addClass('up');";
         }
-        if ( $hide_after != -1)
-        {
+        if ($hide_after != -1) {
             $js_in .= "jQuery('.jBar').delay($hide_after).slideUp(function() {
-                        jQuery('.jRibbon').toggleClass('up').slideDown();
-                    });";
+                         jQuery('.jRibbon').toggleClass('up').slideDown();
+                     });";
         }
 
         $js = "jQuery(document).ready(function() {
-                jQuery('.jBar').hide().delay($show_wait).slideDown(300);
-                $js_in
+                 jQuery('.jBar').hide().delay($show_wait).slideDown(300);
+                 $js_in
 
-                jQuery('.jTrigger').click(function(){
-                    if( jQuery('.jRibbon').hasClass('up') ){
-                        jQuery('.jRibbon').toggleClass('up').slideDown();
-                    }else{
-                        jQuery('.jRibbon').toggleClass('up').slideUp();
-                    }
+                 jQuery('.jTrigger').click(function(){
+                     if( jQuery('.jRibbon').hasClass('up') ){
+                         jQuery('.jRibbon').toggleClass('up').slideDown();
+                     }else{
+                         jQuery('.jRibbon').toggleClass('up').slideUp();
+                     }
 
-                    jQuery('.jBar').slideToggle();
-                });
-            });";
+                     jQuery('.jBar').slideToggle();
+                 });
+             });";
 
         $doc->addScriptDeclaration($js);
     }
 
     function loadjQuery()
     {
-        // $doc = JFactory::getDocument();
         $doc = $this->getApplication()->getDocument();
 
         $version = new Version();
 
-        if ($this->params->get('jquery'))
-        {
+        if ($this->params->get('jquery')) {
             // if its running on J2.5.x then load jquery from module core othwerwise framework library
-            if($version->isCompatible('2.5') && $version->isCompatible( '3.0' ) ) {
+            if ($version->isCompatible('2.5') && $version->isCompatible('3.0')) {
 
                 $jquery = Uri::root(true) . '/plugins/system/jbar/assets/jquery-1.8.2.min.js';
                 $jquery_noconflict = Uri::root(true) . '/plugins/system/jbar/assets/jquery-noconflict.js';
-                // $doc->addScript($jquery);
-                // $doc->addScript($jquery_noconflict);
-                $this->getApplication()->getDocument()->addScript($jquery);
-                // $this->getApplication()->getDocument()->addScriptOptions($jquery_noconflict);
+                $doc->addScript($jquery);
+                $doc->addScript($jquery_noconflict);
 
 
-            } elseif($version->isCompatible('4.0')) {
+            } elseif ($version->isCompatible('4.0')) {
                 $jquery = Uri::root(true) . '/plugins/system/jbar/assets/jquery-3.6.3.min.js';
                 $jquery_noconflict = Uri::root(true) . '/plugins/system/jbar/assets/jquery-noconflict.js';
-                // $doc->addScript($jquery);
-                // $doc->addScript($jquery_noconflict);
-                // $this->getApplication()->getDocument()->addScriptOptions($jquery);
-                // $this->getApplication()->getDocument()->addScriptOptions($jquery_noconflict);
-            } else{
+                $doc->addScript($jquery);
+                $doc->addScript($jquery_noconflict);
+            } else {
                 JHtml::_('jquery.framework');
             }
         }
